bb44d3ac6959e4d336877a0aca51a9b1cba7ab98
wissem lemsi <wissemkill@gmail.com>
2023-10-19 17:44:33 +0100

    test

--- src/main/java/com/example/tp1/controller/ClientController.java
+++ src/main/java/com/example/tp1/controller/ClientController.java
@@ -1,11 +1,16 @@
 package com.example.tp1.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ClientController {
 
-    @GetMapping(value = "template")
-    public String getT
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("message", "Hello, Thymeleaf!");
+        return "index"; // Corresponds to the name of your Thymeleaf template (index.html)
+    }
 }
